218b7c8bb1def19921251740ca3cf420e8ea4ade
Shashank Yadav <ShashankYadav2803@gmail.com>
2026-03-03 19:32:18 +0530

    Initial Commit

---
+++ StreakForge/StreakForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakForgeApp.swift
+//  StreakForge
+//
+//  Created by Shashank Yadav on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct StreakForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
