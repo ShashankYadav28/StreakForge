640de6ec90078536da457e8784bd21fd8c7ad737
Shashank Yadav <ShashankYadav2803@gmail.com>
2026-03-15 17:01:01 +0530

    Modified ADddHabitView: corrected the custom days selection

--- StreakForge/AddHabitView.swift
+++ StreakForge/AddHabitView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct AddHabitView: View {
+//    @ObservedObject var habitVIewModel:HabitViewModel
     @State var title:String = ""
     @State var selectionFrequency:FrequencyType = .daily
-    @State var goal:Int = 1
     @State var selectedDays: Set<Habit.Weekday> = []
+    @State var dailyGoal = 1
+    @State var weeklyGoal = 1
+    @State var customGoal = 1
+    var onSave:(Habit) -> Void
+    
+    // it work like basiclly calling the function throught the envirnment that can dismiss the view and later we can call the function
+    
+    @Environment(\.dismiss) var dismiss
     var body: some View {
         NavigationStack {
             Form {
@@ -27,21 +35,22 @@ struct AddHabitView: View {
                                 .tag(data)
                         }
                     }
+                    
                     .pickerStyle(.segmented)
                 }
                 
                 switch selectionFrequency {
                 case .daily:
                     Section("Goal Per Day"){
-                        Stepper(value:$goal,in: 1...20) {
-                            Text("Daily Goal : \(goal)")
+                        Stepper(value:$dailyGoal,in: 1...20) {
+                            Text("Daily Goal : \(dailyGoal)")
                         }
                         
                     }
                 case .weekly:
                     Section("Goal Per Week"){
-                        Stepper(value:$goal,in: 1...25) {
-                            Text("Weekly Goal : \(goal)")
+                        Stepper(value:$weeklyGoal,in: 1...50) {
+                            Text("Weekly Goal : \(weeklyGoal)")
                         }
                         
                     }
@@ -49,19 +58,55 @@ struct AddHabitView: View {
                 case .custom:
                     Section("Select days") {
                         ForEach(Habit.Weekday.allCases) { data in
-                            
+                            Toggle(data.rawValue.capitalized, isOn: Binding(get: {
+                                selectedDays.contains(data)
+                            }, set: { newValue in
+                                if newValue  {
+                                    selectedDays.insert(data)
+                                }
+                                else {
+                                    selectedDays.remove(data)
+                                }
+                            }))
                             
                         }
                     }
                     Section("Goal Per Day"){
-                        Stepper(value:$goal,in: 0...20) {
-                            Text("Goal : \(goal)")
+                        Stepper(value:$customGoal,in: 1...20) {
+                            Text("Goal : \(customGoal)")
                         }
                     }
                 }
                
                 
             }
+            .toolbar(content: {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel",role: .cancel) {
+                        dismiss()
+                    }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Save",role: .confirm) {
+                        let frequency:Habit.Frequency
+                        switch selectionFrequency {
+                        case .daily:
+                            frequency = .daily(goalsPerday: dailyGoal)
+                        case .weekly:
+                            frequency = .weekly(goalsPerWeek: weeklyGoal)
+                            
+                        case .custom:
+                            frequency = .custom(days: Array(selectedDays), goalPerDay: customGoal)
+                        }
+                        
+                        let cleanTiitle = title.trimmingCharacters(in: .whitespacesAndNewlines)
+                        let habit = Habit(title: cleanTiitle, frequency: frequency)
+                        onSave(habit)
+                        dismiss()
+                    }
+                    .disabled(title.trimmingCharacters(in: .whitespaces).isEmpty)
+                }
+            })
             .navigationTitle("Add Habit")
         }
     }
@@ -80,5 +125,8 @@ enum FrequencyType:String, Identifiable ,CaseIterable{
 
 
 #Preview {
-    AddHabitView()
+    AddHabitView( onSave:  { habit in
+        print("habir is saved")
+        
+    })
 }
