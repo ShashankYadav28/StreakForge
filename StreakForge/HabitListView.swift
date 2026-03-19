0a0517be066025d963dce2051da7067a3e337ef7
Shashank Yadav <ShashankYadav2803@gmail.com>
2026-03-19 15:39:01 +0530

    Modified : Connected Empty view with the ListView

--- StreakForge/HabitListView.swift
+++ StreakForge/HabitListView.swift
@@ -11,35 +11,78 @@ import SwiftUI
 struct HabitListView:View {
     
     @StateObject var habitViewModel:HabitViewModel = HabitViewModel()
-    @State var showHabitSCreen = false
+    @State var showHabitScreen = false
+    @State var showDuplicateAlert = false
     
     var body: some View {
         NavigationStack {
-            List(habitViewModel.habits) { habit in
-                HabitRowView(habitViewModel: habitViewModel, habit: habit)
             
+            if habitViewModel.habits.isEmpty {
+                HabitEmptyView()
+                    .toolbar(content: {
+                        ToolbarItem {
+                            Button {
+                                showHabitScreen = true
+                            }label: {
+                                Image(systemName: "plus")
+                            }
+                        }
+                    })
             }
-            .navigationTitle("Streak Forge")
-            .toolbar(content: {
-                ToolbarItem {
-                    Button {
-                        showHabitSCreen = true
-                    }label: {
-                        Image(systemName: "plus")
+            else {
+                List {
+                    ForEach(habitViewModel.habits) { habit in
+                        HabitRowView(habitViewModel: habitViewModel, habit: habit)
+                         
                     }
+                    .onDelete { indexSet in
+                        let index  = indexSet.first
+                        if let index = index {
+                            let habit  = habitViewModel.habits[index]
+                            habitViewModel.deleteHabit(habitID: habit.id)
+                            
+                        }
+                    }
+                    
+                }
+                .navigationTitle("Streak Forge")
+                .toolbar(content: {
+                    ToolbarItem {
+                        Button {
+                            showHabitScreen = true
+                        }label: {
+                            Image(systemName: "plus")
+                        }
+                    }
+                })
+                
+                
+               
+            }
+         
+            
+       }
+        .alert("Duplicate Habit", isPresented: $showDuplicateAlert, actions: {
+            Button("OK",role: .cancel) {
+                
+            }
+        })
+        .sheet(isPresented: $showHabitScreen, content: {
+            AddHabitView(onSave: { habit in
+                let result = habitViewModel.addHabit(habit: habit)
+                if result ==  false {
+                    showHabitScreen = false
+                    showDuplicateAlert = true
                 }
             })
-       }
-        .sheet(isPresented: $showHabitSCreen, content: {
-            Text("HabitScreen ")
         })
-        .onAppear {
-            habitViewModel.habits = [
-                Habit(title: "Drink Water", frequency: .daily(goalsPerday: 8)),
-                 Habit(title: "Workout", frequency: .custom(days: [.monday,.wednesday,.friday], goalPerDay: 1)),
-                 Habit(title: "ReadBooK", frequency: .weekly(goalsPerWeek: 9))
-            ]
-        }
+//        .onAppear {
+//            habitViewModel.habits = [
+//                Habit(title: "Drink Water", frequency: .daily(goalsPerday: 8)),
+//                 Habit(title: "Workout", frequency: .custom(days: [.monday,.wednesday,.friday], goalPerDay: 1)),
+//                 Habit(title: "ReadBooK", frequency: .weekly(goalsPerWeek: 9))
+//            ]
+//        }
         
     }
 }
